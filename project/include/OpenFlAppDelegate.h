3cb1a197a2edeb223fb41dc6e6c9292c02d4b4a7
Federico Vidueiro <vidueirof@MacBook-Air-de-Federico-2.local>
2015-12-28 15:20:01 -0300

    Added needed header from other extension, need to find a way to avoid doing this

--- project/include/OpenFlAppDelegate.h
+++ project/include/OpenFlAppDelegate.h
@@ -1,1 +1,14 @@
-../../../extension-ios-app-delegate/project/include/OpenFlAppDelegate.h
+#import <UIKit/UIKit.h>
+
+@class OpenFlAppDelegate;
+
+@interface OpenFlAppDelegate : NSObject <UIApplicationDelegate>
+
+- (void)addAppDelegate:(id)delegate;
+
+@property (nonatomic, retain) NSMutableArray *delegates;
+
++ (id)sharedInstance;
+
+@end
+
